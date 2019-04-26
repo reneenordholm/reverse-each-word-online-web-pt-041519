d5f1558592ad78e7d207ac394bb68677601c698c
Renee Nordholm <reneenordholm@gmail.com>
2019-04-26 14:26:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -18,7 +18,7 @@ require 'pry'
 def reverse_each_word(sentence1)
   convert_to_string = sentence1.split(/ /) #converted array to string w/words separated
   new_sentence = [] #creating new array for updated sentence
-    convert_to_string.each do |word| #going through each word in the string
+    convert_to_string.map do |word| #going through each word in the string
      new_sentence << word.reverse #reverse each word in the string, add to new array#  end
       puts new_sentence.join(" ") #convert updated sentence to string
  end 
