dadc3094ecbd0773e16796f5e78b055ceae3c1be
Renee Nordholm <reneenordholm@gmail.com>
2019-04-26 14:08:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@ require 'pry'
 def reverse_each_word(sentence1)
   new_sentence = sentence1.split(/ /)
     new_sentence.each do |word|
-     word.reverse
+     word.reverse.join
   end
     return new_sentence
 end
